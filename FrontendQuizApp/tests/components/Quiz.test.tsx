324092c8508843c4d84080e083f964e8d009a637
Stephen Adu <stephen.adu@amalitech.com>
2024-12-19 09:41:06 +0000

    (test):Additional test for the Quiz components
    More tests are added to the Quiz components and new test were created for the Quiz Completed component

--- FrontendQuizApp/tests/components/Quiz.test.tsx
+++ FrontendQuizApp/tests/components/Quiz.test.tsx
@@ -7,70 +7,79 @@ import { QuizProps } from "../../src/components/Quiz/Quiz.component";
 import { Questions } from "../../types";
 
 describe("Quiz component", () => {
-  it("should render questions and the answers correctly", () => {
-    // Define mock questions with the shape of Questions type
-    const mockQuestions: Questions[] = [
-      {
-        question: "What is the capital of France?",
-        options: ["Berlin", "Madrid", "Paris", "Rome"],
-        answer: "Paris",
-      },
+  const mockQuestions: Questions[] = [
+    {
+      question: "What is the capital of France?",
+      options: ["Berlin", "Madrid", "Paris", "Rome"],
+      answer: "Paris",
+    },
 
-      {
-        question: "Which planet is known as the Red Planet?",
-        options: ["Earth", "Mars", "Venus", "Jupiter"],
-        answer: "Mars",
-      },
-    ];
+    {
+      question: "Which planet is known as the Red Planet?",
+      options: ["Earth", "Mars", "Venus", "Jupiter"],
+      answer: "Mars",
+    },
+  ];
 
-    // Create the quizProps with mock data for testing
-    const quizProps: QuizProps = {
-      questions: mockQuestions,
-      setHasCompleted: vi.fn(),
-      setScore: vi.fn(),
-      score: 0,
-    };
+  // Create the quizProps with mock data for testing
+  const quizProps: QuizProps = {
+    questions: mockQuestions,
+    setHasCompleted: vi.fn(),
+    setScore: vi.fn(),
+    score: 0,
+  };
 
-    // Render the Quiz component with the mock questions
+  it("should render question and the answers correctly", () => {
     render(<Quiz {...quizProps} />);
-
-    expect(
-      screen.getByText("What is the capital of France?")
-    ).toBeInTheDocument();
-
+    expect(screen.getByText(mockQuestions[0].question)).toBeInTheDocument();
     mockQuestions[0].options.forEach((option) => {
       expect(screen.getByText(option)).toBeInTheDocument();
       fireEvent.click(screen.getByText(option));
     });
   });
-  it("should handle answer submission ", () => {
-    // Define mock questions
-    const mockQuestions: Questions[] = [
-      {
-        question: "What is the capital of France?",
-        options: ["Berlin", "Madrid", "Paris", "Rome"],
-        answer: "Paris",
-      },
-      {
-        question: "Which planet is known as the Red Planet?",
-        options: ["Earth", "Mars", "Venus", "Jupiter"],
-        answer: "Mars",
-      },
-    ];
 
-    // Create mock functions for handling completion and score
-    const mockSetHasCompleted = vi.fn();
-    const mockSetScore = vi.fn();
-
-    // Set up the initial score and render the component
-    const quizProps: QuizProps = {
-      questions: mockQuestions,
-      setHasCompleted: mockSetHasCompleted,
-      setScore: mockSetScore,
-      score: 0,
-    };
-
+  it("the submit answer button should be visible on the screen together with the options", () => {
     render(<Quiz {...quizProps} />);
+    expect(
+      screen.getByRole("button", { name: /Submit Answer/i })
+    ).toBeInTheDocument();
+  });
+
+  it("should show that the answer is highlighted", () => {
+    render(<Quiz {...quizProps} />);
+    const allHeadings = screen.getAllByRole("button");
+    const firstAnswer = allHeadings[2];
+    const secondAnswer = allHeadings[3];
+    expect(firstAnswer).toBeVisible();
+    expect(secondAnswer).toBeVisible();
+    fireEvent.click(firstAnswer);
+    expect(firstAnswer).toHaveStyle("background: 2px solid #A729F5 ");
+    fireEvent.click(secondAnswer);
+    expect(secondAnswer).toHaveStyle("background: 2px solid #A729F5 ");
+  });
+
+  it("should display the next question button and the correct answer before submitting an answer", () => {
+    render(<Quiz {...quizProps} />);
+    fireEvent.click(screen.getByText("Paris"));
+    fireEvent.click(screen.getByRole("button", { name: /Submit/i }));
+    expect(screen.getByRole("button", { name: /Next/i }));
+    const icon_img = screen.getByRole("img");
+
+    expect(icon_img).toHaveAttribute("src", "/assets/images/icon-correct.svg");
+  });
+
+  it("should see the next question and question number", () => {
+    render(<Quiz {...quizProps} />);
+
+    expect(screen.getByText(/Question 2 of/i)).toBeInTheDocument();
+    expect(screen.getByText(mockQuestions[1].question)).toBeInTheDocument();
+  });
+
+  it(`should display "Please select an answer" when the submit button is clicked with no answer selected`, () => {
+    render(<Quiz {...quizProps} />);
+
+    fireEvent.click(screen.getByRole("button", { name: /Submit/i }));
     screen.debug();
+    expect(screen.getByText(/Please select/i)).toBeInTheDocument();
   });
 });
